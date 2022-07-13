e963df3be62440d58c2c7c178f6113e7bc155770
Chris LaRocque <larocque.christopher@gmail.com>
2022-07-13 15:17:06 -0400

    Initial hackin

---
+++ gatsby-node.js
@@ -0,0 +1,65 @@
+const componentWithMDXScope = require('gatsby-plugin-mdx/component-with-mdx-scope');
+
+const path = require('path');
+
+const startCase = require('lodash.startcase');
+
+const config = require('./config');
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      alias: {
+        $components: path.resolve(__dirname, 'src/components'),
+        buble: '@philpl/buble', // to reduce bundle size
+      },
+    },
+  });
+};
+
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPlugin({
+    name: '@babel/plugin-proposal-export-default-from',
+  });
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `Mdx`) {
+    const parent = getNode(node.parent);
+
+    let value = parent.relativePath.replace(parent.ext, '');
+
+    if (value === 'index') {
+      value = '';
+    }
+
+    if (config.gatsby && config.gatsby.trailingSlash) {
+      createNodeField({
+        name: `slug`,
+        node,
+        value: value === '' ? `/` : `/${value}/`,
+      });
+    } else {
+      createNodeField({
+        name: `slug`,
+        node,
+        value: `/${value}`,
+      });
+    }
+
+    createNodeField({
+      name: 'id',
+      node,
+      value: node.id,
+    });
+
+    createNodeField({
+      name: 'title',
+      node,
+      value: node.frontmatter.title || startCase(parent.name),
+    });
+  }
+};
